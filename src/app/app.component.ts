4787552a89945406c729272a74b302111d38ced2
Mamadou Diallo <diallomamadoud1919@gmail.com>
2024-12-20 17:19:11 +0100

    Dynamisation de l'affichage de la localisation avec les control flow block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,38 +9,42 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap !: FaceSnap
-  myOtherSnap !: FaceSnap
-  myLastSnap !: FaceSnap
+  faceSnaps !: FaceSnap[]
+
+  // mySnap !: FaceSnap
+  // myOtherSnap !: FaceSnap
+  // myLastSnap !: FaceSnap
 
   ngOnInit(): void {
 
-    //Instance d'une faceSnap
-    this.mySnap = new FaceSnap(
-      'Mbeur',
-      'He is my best friend of all times !!!',
-      'https://static6.depositphotos.com/1000792/646/v/450/depositphotos_6464182-stock-illustration-two-boys-go-to-school.jpg',
-      new Date(),
-      0
-    )
+    //Instances de faceSnap
+    this.faceSnaps = [
+      new FaceSnap(
+        'Mbeur',
+        'He is my best friend of all times !!!',
+        'https://static6.depositphotos.com/1000792/646/v/450/depositphotos_6464182-stock-illustration-two-boys-go-to-school.jpg',
+        new Date(),
+        0
+      ),
 
-    this.myOtherSnap = new FaceSnap(
-      'Cousin 4',
-      'they are my best team of all times !!!',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbkNFUeiy5MrMhbF2jhK_qMGpHK-3Rmk2-dA&s',
-      new Date(),
-      0
-    )
+      new FaceSnap(
+        'Cousin 4',
+        'they are my best team of all times !!!',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbkNFUeiy5MrMhbF2jhK_qMGpHK-3Rmk2-dA&s',
+        new Date(),
+        0
+      ),
 
-    this.myLastSnap = new FaceSnap(
-      'Foot',
-      'It is my favorite game of all times !!!',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr56dMjTTikb7JgAZrkRsCgg44fDUedvTHAQ&s',
-      new Date(),
-      0
-    )
-    //Ajout d'une localisation à l'instance myOtherSnap
-    this.myOtherSnap.setLocation('every where')
+      new FaceSnap(
+        'Foot',
+        'It is my favorite game of all times !!!',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTr56dMjTTikb7JgAZrkRsCgg44fDUedvTHAQ&s',
+        new Date(),
+        0
+      )
+    ]
+    //Ajout d'une localisation à l'instance situé à l'index 1
+    this.faceSnaps[1].setLocation('every where')
 
   }
 }

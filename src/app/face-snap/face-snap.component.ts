3228863927c6e2c954fad7d3af1fd461688b864d
Mamadou Diallo <diallomamadoud1919@gmail.com>
2025-01-14 14:06:04 +0100

    Découvert des pipes – des outils pour gérer le format d'affichage d'une donnée

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,11 @@
-import { NgClass, NgStyle } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 import { FaceSnap } from './../models/face-snap';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',  // balise html personnalisée qu'on utisera pour inserer 
   // le contenu html de ce compenent dans d'autre components
-  imports: [NgStyle, NgClass], //tableau d'import des dépendances
+  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe], //tableau d'import des dépendances
   templateUrl: './face-snap.component.html', //indique le fichier template
   styleUrl: './face-snap.component.scss' // indique le fichier style du component
 })
